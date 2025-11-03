e39cf6f85841db477e89344f18e13de542e6f4a1
Bill-linho <billlinho37@gmail.com>
2025-11-03 16:23:32 -0300

    9

--- services/Recipes.servece.js
+++ services/Recipes.servece.js
@@ -4,7 +4,12 @@ async function getRecipes() {
     const request = `${url}/receitas`
 
     try {
-        const resposta = await fetch(request,)
+        const response = await fetch(request,{
+            method: 'GET'
+        })
+        const data = response.json()
+        console.log(data)
+        
     } catch (e) {
         
     }
